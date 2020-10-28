759783dc5d138b5ac83e498acd90c7a275f2ddf2
Lucía <uo272830@uniovi.es>
2020-10-28 19:21:27 +0100

    section about

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-/*const nav = document.getElementsByTagName('nav')[0]*/
-const nav = document.getElementsByClassName('nav-list')[0]
-
-toggleButton.addEventListener('click', () => {
-    nav.classList.toggle('active');
-})
